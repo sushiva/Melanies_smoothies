03ea632f3837e2df7eabc177abba41fafa43d817
sushiva <sukalpa15@gmail.com>
2025-09-16 10:55:49 -0400

    Update streamlit_app.py
    Modified the code to get the smoothieFroot data to show nutrition for the Fruits chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,23 +32,17 @@ ingradients_list = st.multiselect(
 if ingradients_list:
     ingredients_string = '' 
     for fruit_chosen in ingradients_list:
-        ingredients_string += fruit_chosen +' '    
-    # st.write(ingredients_string)    
-  ##  my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-  ##          values ('""" + ingredients_string + """')"""
+        ingredients_string += fruit_chosen +' '      
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-    #st.write(my_insert_stmt)
-   
-
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-# New section to display smoothiefruit nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 
