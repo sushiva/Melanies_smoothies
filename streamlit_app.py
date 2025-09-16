4f6111c59a571311e33880deabba2148fbac6e87
sushiva <sukalpa15@gmail.com>
2025-09-16 12:06:28 -0400

    Update streamlit_app.py
    code changes to use Pandas Dataframe to search on values

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe =  session.table("smoothies.public.fruit_options").select(col('FRUI
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingradients_list = st.multiselect(
     "Chose upto 5 ingradients",    
@@ -39,7 +39,11 @@ if ingradients_list:
     ingredients_string = '' 
     for fruit_chosen in ingradients_list:
         ingredients_string += fruit_chosen +' '      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + 'Nutrition Information')
+      
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
